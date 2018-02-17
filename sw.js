4cade643789652817ffcf946e9783687d3aeb857
pankajladhar <ladharpankaj@gmail.com>
2018-02-17 09:09:05 +0530

    Inital commit

---
+++ sw.js
@@ -0,0 +1,13 @@
+// self.addEventListener('install', function (event) {
+//    // console.log('%c ServiceWorker0 installation successful', 'color: #FF00ff');
+//     //event.waitUntil(self.skipWaiting());
+
+//     /* pre cache tasks can be done here */
+// });
+
+// self.addEventListener('activate', function (event) {
+//    // console.log('%c ServiceWorker0 activation successful', 'color: #CDDC39');
+//     //event.waitUntil(self.clients.claim());
+
+//     /* clearing old caches tasks can be done here */
+// });
